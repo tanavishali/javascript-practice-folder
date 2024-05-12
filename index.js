351248a4f60b170d945ec954d63fbe17fdf768f8
tanavishali <tanawishalrai5271@gmail.com>
2024-05-12 20:06:58 +0500

    nothing

---
+++ index.js
@@ -0,0 +1,138 @@
+// // // let number=83738
+// // // let name2='s'
+// // // let age;
+// // let sum=null
+// // let x=false;
+
+// // // console.table([ typeof name2, typeof age, typeof sum])
+
+// // // console.log( typeof x);
+
+// // let y= 'left' + 16 + 2 +  'ali' +3
+// // // console.log(y);
+
+// // document.getElementById('demo').innerHTML=y
+
+// // let z =2938;    // 12300000
+// // let ab=new String(2938);
+// // console.log(typeof ab);
+
+// // let number=BigInt(7228291919282930282922)
+// // console.log(typeof number);
+// // // object
+// // const person={
+// // firstName:"aa\vd",
+// // lastName:"asad",
+// // age:33
+// // }
+
+// // console.log(person.firstName  ,person.lastName, person.age);
+
+// // // console.log(typeof person.firstName);
+// let first_name="tanavish"
+// let middle_name="ali"
+// let last_name="alrai    "
+// let full_name=first_name + " "+middle_name +" "+last_name
+// console.log(full_name);
+// console.log(first_name[5]);
+// console.log(full_name.slice(0,13));
+// console.log(full_name.length);
+// console.log(full_name.toUpperCase().slice(0,26));
+// console.log(full_name.toLowerCase().slice(0,13));
+// console.log(full_name.trim())
+// // repeat the string
+// console.log(full_name.repeat(5).toUpperCase());
+// console.log(first_name.trim()); trim used for remove white spaces
+// console.log(first_name.replace('v','w'));
+// let x=1.1
+// let y=2.2
+// let sum=x+y
+// let z=(x*10 +y*10)/10
+// console.log(sum.toFixed(2));
+// console.table([ z]);
+// let a=100
+// let b="19"
+// let c=a/b
+// console.log(c );
+// let score="33ssss"
+// console.log(typeof score);
+// console.log(score);
+// let stringtonumber= Number(score)
+// console.log(stringtonumber);
+// console.log(typeof stringtonumber);
+// let numbertostring=String(stringtonumber)
+// console.log(typeof numbertostring,numbertostring);
+// console.log(typeof NaN);
+// let age=22
+// console.log( typeof age);
+// let ageinstring=String(age)
+// console.log(typeof ageinstring);
+// let ageinnumber= Number(ageinstring)
+// console.log( typeof ageinnumber);
+// let a=11;
+// // console.log(a);
+// let b=String(a)
+// console.log(typeof b);
+// console.table([a,b,typeof b]);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+// console.log(2-5+" 4 "+3);
+// let a=2+2+"4"+8+3
+// console.log(a);
+// console.log(typeof a);
+// let count=33
+// count++
+// console.log(count);
+// ++count
+// count++
+// console.log(count);
+// let x=3
+// console.log(x);
+// let y=++x
+// console.table([x,y]);
+// console.log("03"==1);
+// if (3>1) {
+//     console.log(true);
+// } else {
+//     console.log(false);
+// }
+// let a=null
+// console.log(typeof a);
+// for (let index = 0; index < 3; index++) {
+//     const element = array[index];
+//     console.log(index);
+//     alert(index)
+// }
+// let name="tanawish"
+// console.log(name);
+// let nametwo=name;
+// console.log(nametwo);
+// nametwo="ali"
+// console.log(nametwo);
+// let person={
+//     name:"tanawish",
+
+// }
+// let changename=person.name="ali"
+// console.log(change name);
+// console.log(person.name);
+// let name= ne"tanawish"
+// const name = new String("arguments");
+// console.log(name);
+// let name="tanawish"
+//  let name2=name[4]="v"
+// console.log(name2);
+// let name1={
+//     name:"tanawish"
+// }
+
+// let name2=name1.name[4]="v"
+// console.log(name1.name);
+// console.log(name2);
+// let name="ali"
+// const name2 = new Number(name);
+// console.log( name2);
+// console.log(name2.indexof());
